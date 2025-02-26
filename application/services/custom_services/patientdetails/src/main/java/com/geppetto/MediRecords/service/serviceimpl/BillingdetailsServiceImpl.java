576aef62255c53ab6335c4250506159bd28716bc
Arun-YA <arun.ya@10decoders.in>
2025-02-26 14:58:41 +0530

    Replaced builder instead of using beanutils

--- application/services/custom_services/patientdetails/src/main/java/com/geppetto/MediRecords/service/serviceimpl/BillingdetailsServiceImpl.java
+++ application/services/custom_services/patientdetails/src/main/java/com/geppetto/MediRecords/service/serviceimpl/BillingdetailsServiceImpl.java
@@ -1,178 +1,182 @@
 package com.geppetto.MediRecords.service.serviceimpl;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
 import com.geppetto.MediRecords.dao.BillingdetailsDao;
 import com.geppetto.MediRecords.dto.BillingdetailsDto;
 import com.geppetto.MediRecords.exception.EntityNotFoundException;
+// import com.geppetto.MediRecords.model.Appointment;
 import com.geppetto.MediRecords.model.Billingdetails;
-import com.geppetto.MediRecords.service.BillingdetailsService;
-import java.util.List;
-import org.springframework.data.domain.PageRequest;
-import java.util.Map;
-import org.springframework.data.domain.Page;
-import com.geppetto.MediRecords.util.ConstructQuery;
-import org.springframework.data.domain.Pageable;
 import com.geppetto.MediRecords.repository.BillingdetailsRepository;
-import java.util.stream.Collectors;
-import org.springframework.data.jpa.domain.Specification;
+import com.geppetto.MediRecords.service.BillingdetailsService;
+import com.geppetto.MediRecords.util.PatientDetailsUtil;
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Service;
-import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
-* Implementation of the {@link BillingdetailsService} interface.
-* Provides services related to Billingdetails, including CRUD operations and file uploads/downloads.
-*/
-
+ * Implementation of the {@link BillingdetailsService} interface. Provides
+ * services related to Billingdetails, including CRUD operations and file
+ * uploads/downloads.
+ */
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class BillingdetailsServiceImpl implements BillingdetailsService {
 
-
     /**
-    * Constructs a {@code BillingdetailsServiceImpl} with the specified DAO.
-    *
-    * @param billingdetailsDao The DAO for accessing the data.
-    */
+     * Constructs a {@code BillingdetailsServiceImpl} with the specified DAO.
+     *
+     * @param billingdetailsDao The DAO for accessing the data.
+     */
     private final BillingdetailsDao billingdetailsDao;
     private final BillingdetailsRepository billingdetailsRepository;
-    private final ConstructQuery constructQuery;
+    private final PatientDetailsUtil patientDetailsUtil;
 
-  
     /**
-    * Creates new billingdetails.
-    *
-    * @param billingdetailsDto The {@link BillingdetailsDto} to be created.
-    * @return The created {@link BillingdetailsDto}.
-    */
+     * Creates new billingdetails.
+     *
+     * @param billingdetailsDto The {@link BillingdetailsDto} to be created.
+     * @return The created {@link BillingdetailsDto}.
+     */
     @Override
-    public BillingdetailsDto  createBillingdetails(BillingdetailsDto billingdetailsDto) {
-    log.info("Entering createBillingdetails method");
-    Billingdetails billingdetails = new Billingdetails();
-    BeanUtils.copyProperties(billingdetailsDto, billingdetails);
-    Billingdetails createdBillingdetails = billingdetailsDao.createBillingdetails(billingdetails);
-    BeanUtils.copyProperties(createdBillingdetails, billingdetailsDto);
-    log.info("Exiting createBillingdetails method");
-    return billingdetailsDto;
+    public BillingdetailsDto createBillingdetails(BillingdetailsDto billingdetailsDto) {
+        log.info("Entering createBillingdetails method");
+
+        Billingdetails billingdetails = patientDetailsUtil.toEntity(billingdetailsDto);
+        Billingdetails createdBillingdetails = billingdetailsDao.createBillingdetails(billingdetails);
+        billingdetailsDto = patientDetailsUtil.toDto(createdBillingdetails);
+
+        log.info("Exiting createBillingdetails method");
+        return billingdetailsDto;
     }
-  
+
     /**
-    * Retrieves billingdetails by its ID.
-    *
-    * @param id The ID of the billingdetails to retrieve. Must not be {@code null}.
-    * @return The billingdetails data transfer object associated with the specified ID.
-    * @throws EntityNotFoundException If no billingdetails with the specified ID is found.
-    */
+     * Retrieves billingdetails by its ID.
+     *
+     * @param id The ID of the billingdetails to retrieve. Must not be
+     * {@code null}.
+     * @return The billingdetails data transfer object associated with the
+     * specified ID.
+     * @throws EntityNotFoundException If no billingdetails with the specified
+     * ID is found.
+     */
     @Override
-    public BillingdetailsDto  getBillingdetailsById(String id) {
-    log.info("Entering getBillingdetailsById method for ID: {}", id);
-    return billingdetailsDao.getBillingdetailsById(id)
-        .map(billingdetails -> {
-            BillingdetailsDto dto = new BillingdetailsDto();
-            BeanUtils.copyProperties(billingdetails, dto);
-            log.info("Exiting getBillingdetailsById method for ID: {}", id);
+    public BillingdetailsDto getBillingdetailsById(String id) {
+        log.info("Entering getBillingdetailsById method for ID: {}", id);
+        return billingdetailsDao.getBillingdetailsById(id)
+                .map(billingdetails -> {
+                    BillingdetailsDto dto = patientDetailsUtil.toDto(billingdetails);
+                    log.info("Exiting getBillingdetailsById method for ID: {}", id);
+                    return dto;
+                })
+                .orElseThrow(() -> {
+                    log.warn("No billingdetails found for ID: {}", id);
+                    return new EntityNotFoundException("Data not found for ID: " + id);
+                });
+    }
+
+    /**
+     * Retrieves all billingdetails.
+     *
+     * @return A list of {@link BillingdetailsDto} representing all
+     * billingdetails.
+     */
+    @Override
+    public Page<BillingdetailsDto> getAllBillingdetails(int page, int size) {
+        log.info("Entering getAllBillingdetails method");
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Billingdetails> billingdetailsPage = billingdetailsDao.getAllBillingdetails(pageable);
+        Page<BillingdetailsDto> billingdetailsDtoPage = billingdetailsPage.map(billingdetails -> {
+            BillingdetailsDto dto = patientDetailsUtil.toDto(billingdetails);
             return dto;
-        })
-        .orElseThrow(() -> {
-            log.warn("No billingdetails found for ID: {}", id);
-            return new EntityNotFoundException("Data not found for ID: " + id);
         });
+        log.info("Exiting getAllBillingdetails method");
+        return billingdetailsDtoPage;
     }
-  
+
     /**
-    * Retrieves all billingdetails.
-    *
-    * @return A list of {@link BillingdetailsDto} representing all billingdetails.
-    */
+     * Searches for billingdetails based on provided parameters.
+     *
+     * @param allParams A map of search parameters.
+     * @return A list of {@link BillingdetailsDto} matching the search
+     * parameters.
+     */
     @Override
-    public Page<BillingdetailsDto>  getAllBillingdetails(int page, int size) {
-    log.info("Entering getAllBillingdetails method");
-    Pageable pageable = PageRequest.of(page, size);
-    Page<Billingdetails> billingdetailsPage = billingdetailsDao.getAllBillingdetails(pageable);
-    Page<BillingdetailsDto> billingdetailsDtoPage = billingdetailsPage.map(billingdetails -> {
-        BillingdetailsDto dto = new BillingdetailsDto();
-        BeanUtils.copyProperties(billingdetails, dto);
-        return dto;
-    });
-    log.info("Exiting getAllBillingdetails method");
-    return billingdetailsDtoPage;
+    public List<BillingdetailsDto> searchBillingdetails(Map<String, String> allParams) {
+        log.info("Entering searchBillingdetails method for SQL");
+
+        Specification<Billingdetails> specification = patientDetailsUtil.constructSearchQuery(allParams, Billingdetails.class);
+        List<Billingdetails> results = billingdetailsRepository.findAll(specification);
+        List<BillingdetailsDto> billingdetailsDtos = results.stream()
+                .map(billingdetails -> {
+                    BillingdetailsDto dto = patientDetailsUtil.toDto(billingdetails);
+                    return dto;
+                })
+                .collect(Collectors.toList());
+
+        log.info("Exiting searchBillingdetails method for SQL. Results found: {}", billingdetailsDtos.size());
+        return billingdetailsDtos;
     }
-  
+
     /**
-    * Searches for billingdetails based on provided parameters.
-    *
-    * @param allParams A map of search parameters.
-    * @return A list of {@link BillingdetailsDto} matching the search parameters.
-    */
+     * Updates existing billingdetails.
+     *
+     * @param billingdetailsDto The {@link BillingdetailsDto} containing updated
+     * information.
+     * @return The updated {@link BillingdetailsDto}.
+     * @throws EntityNotFoundException If no billingdetails with the specified
+     * ID is found.
+     */
     @Override
-    public List<BillingdetailsDto>  searchBillingdetails(Map<String, String> allParams) {
-    log.info("Entering searchBillingdetails method for SQL");
+    public BillingdetailsDto updateBillingdetails(BillingdetailsDto billingdetailsDto) {
+        log.info("Entering updateBillingdetails method for ID: {}", billingdetailsDto.getId());
 
-    Specification<Billingdetails> specification = constructQuery.constructSearchQuery(allParams, Billingdetails.class);
-    List<Billingdetails> results = billingdetailsRepository.findAll(specification);
-    List<BillingdetailsDto> billingdetailsDtos = results.stream()
-        .map(billingdetails -> {
-            BillingdetailsDto dto = new BillingdetailsDto();
-            BeanUtils.copyProperties(billingdetails, dto);
-            return dto;
-        })
-        .collect(Collectors.toList());
-
-    log.info("Exiting searchBillingdetails method for SQL. Results found: {}", billingdetailsDtos.size());
-    return billingdetailsDtos;
+        return billingdetailsDao.getBillingdetailsById(billingdetailsDto.getId())
+                .map(existingBillingdetails -> {
+                    existingBillingdetails = patientDetailsUtil.toEntity(billingdetailsDto);
+                    Billingdetails updatedBillingdetails = billingdetailsDao.createBillingdetails(existingBillingdetails);
+                    BillingdetailsDto responseDto = patientDetailsUtil.toDto(updatedBillingdetails);
+                    log.info("Exiting updateBillingdetails method for ID: {}", billingdetailsDto.getId());
+                    return responseDto;
+                })
+                .orElseThrow(() -> {
+                    log.warn("No billingdetails found for update with ID: {}", billingdetailsDto.getId());
+                    return new EntityNotFoundException("Data not found for update with ID: " + billingdetailsDto.getId());
+                });
     }
-  
+
     /**
-    * Updates existing billingdetails.
-    *
-    * @param billingdetailsDto The {@link BillingdetailsDto} containing updated information.
-    * @return The updated {@link BillingdetailsDto}.
-    * @throws EntityNotFoundException If no billingdetails with the specified ID is found.
-    */
+     * Deletes billingdetails by ID.
+     *
+     * @param id The ID of the billingdetails to delete.
+     * @return A message indicating the result of the deletion.
+     * @throws EntityNotFoundException If no billingdetails with the specified
+     * ID is found.
+     */
     @Override
-    public BillingdetailsDto  updateBillingdetails(BillingdetailsDto billingdetailsDto) {
-    log.info("Entering updateBillingdetails method for ID: {}", billingdetailsDto.getId());
-    return billingdetailsDao.getBillingdetailsById(billingdetailsDto.getId())
-        .map(existingBillingdetails -> {
-            BeanUtils.copyProperties(billingdetailsDto, existingBillingdetails);
-            Billingdetails updatedBillingdetails = billingdetailsDao.createBillingdetails(existingBillingdetails);
-            BillingdetailsDto responseDto = new BillingdetailsDto();
-            BeanUtils.copyProperties(updatedBillingdetails, responseDto);
-            log.info("Exiting updateBillingdetails method for ID: {}", billingdetailsDto.getId());
-            return responseDto;
-        })
-        .orElseThrow(() -> {
-            log.warn("No billingdetails found for update with ID: {}", billingdetailsDto.getId());
-            return new EntityNotFoundException("Data not found for update with ID: " + billingdetailsDto.getId());
-        });
+    public String deleteBillingdetails(String id) {
+        log.info("Entering deleteBillingdetails method for ID: {}", id);
+
+        billingdetailsDao.getBillingdetailsById(id)
+                .orElseThrow(() -> {
+                    log.warn("No billingdetails found with ID: {}. Deletion failed.", id);
+                    return new EntityNotFoundException("No billingdetails found with ID: " + id + ". Unable to delete.");
+                });
+
+        billingdetailsDao.deleteBillingdetails(id);
+        log.info("Successfully deleted Billingdetails with ID: {}", id);
+
+        return "Billingdetails deleted successfully";
     }
-  
-    /**
-    * Deletes billingdetails by ID.
-    *
-    * @param id The ID of the billingdetails to delete.
-    * @return A message indicating the result of the deletion.
-    * @throws EntityNotFoundException If no billingdetails with the specified ID is found.
-    */
-    @Override
-    public String  deleteBillingdetails(String id) {
-    log.info("Entering deleteBillingdetails method for ID: {}", id);
 
-    Billingdetails billingdetails = billingdetailsDao.getBillingdetailsById(id)
-         .orElseThrow(() -> {
-             log.warn("No billingdetails found with ID: {}. Deletion failed.", id);
-             return new EntityNotFoundException("No billingdetails found with ID: " + id + ". Unable to delete.");
-         });
-
-    billingdetailsDao.deleteBillingdetails(id);
-    log.info("Successfully deleted Billingdetails with ID: {}", id);
-
-       return "Billingdetails deleted successfully";
-    }
-  
 }
-
-
-
---
+++ application/services/custom_services/patientdetails/src/main/java/com/geppetto/MediRecords/util/PatientDetailsUtil.java
@@ -0,0 +1,98 @@
+package com.geppetto.MediRecords.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import com.geppetto.MediRecords.dto.AppointmentDto;
+import com.geppetto.MediRecords.dto.BillingdetailsDto;
+import com.geppetto.MediRecords.model.Appointment;
+import com.geppetto.MediRecords.model.Billingdetails;
+
+import jakarta.persistence.criteria.Predicate;
+
+@Component
+public class PatientDetailsUtil {
+
+    /**
+    * Constructs a JPA Specification query for dynamic search based on the
+    * given parameters.
+    *
+    * @param allParams A map containing query parameters (field name as key,
+    * search value as value).
+    * @return A JPA Specification that can be used to filter results
+    * dynamically.
+    */
+    public <T> Specification<T> constructSearchQuery(Map<String, String> allParams, Class<T> entityClass) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            allParams.forEach((key, value) -> {
+                if (value != null && !value.isEmpty()) {
+                    try {
+                        Class<?> fieldType = entityClass.getDeclaredField(key).getType();
+
+                        if (fieldType.equals(String.class)) {
+                            predicates.add(criteriaBuilder.like(root.get(key), "%" + value + "%"));
+                        } else if (fieldType.equals(Integer.class) || fieldType.equals(int.class)) {
+                            predicates.add(criteriaBuilder.equal(root.get(key), Integer.parseInt(value)));
+                        } else if (fieldType.equals(Long.class) || fieldType.equals(long.class)) {
+                            predicates.add(criteriaBuilder.equal(root.get(key), Long.parseLong(value)));
+                        } else if (fieldType.equals(Boolean.class) || fieldType.equals(boolean.class)) {
+                            predicates.add(criteriaBuilder.equal(root.get(key), Boolean.parseBoolean(value)));
+                        } else if (fieldType.equals(Double.class) || fieldType.equals(double.class)) {
+                            predicates.add(criteriaBuilder.equal(root.get(key), Double.parseDouble(value)));
+                        } else if (fieldType.equals(Float.class) || fieldType.equals(float.class)) {
+                            predicates.add(criteriaBuilder.equal(root.get(key), Float.parseFloat(value)));
+                        } else {
+                            predicates.add(criteriaBuilder.equal(root.get(key), value));
+                        }
+                    } catch (NoSuchFieldException e) {
+                        System.out.print("NoSuchFieldException:" + e);
+                    }
+                }
+            });
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+
+    public Appointment toEntity(AppointmentDto appointmentDto) {
+        return Appointment.builder()
+                .id(appointmentDto.getId())
+                .appointmentid(appointmentDto.getAppointmentid())
+                .patientid(appointmentDto.getPatientid())
+                .doctorname(appointmentDto.getDoctorname())
+                .build();
+    }
+
+    public AppointmentDto toDto(Appointment appointment) {
+        return AppointmentDto.builder()
+                .id(appointment.getId())
+                .appointmentid(appointment.getAppointmentid())
+                .patientid(appointment.getPatientid())
+                .doctorname(appointment.getDoctorname())
+                .build();
+    }
+
+    public Billingdetails toEntity(BillingdetailsDto billingdetailsDto) {
+        return Billingdetails.builder()
+                .id(billingdetailsDto.getId())
+                .billingid(billingdetailsDto.getBillingid())
+                .patientid(billingdetailsDto.getPatientid())
+                .paymentstatus(billingdetailsDto.getPaymentstatus())
+                .build();
+    }
+
+    public BillingdetailsDto toDto(Billingdetails billingdetails) {
+        return BillingdetailsDto.builder()
+                .id(billingdetails.getId())
+                .billingid(billingdetails.getBillingid())
+                .patientid(billingdetails.getPatientid())
+                .paymentstatus(billingdetails.getPaymentstatus())
+                .build();
+    }
+
+}
